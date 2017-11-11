341ffc0b6a70d734c44b05ed3f41ab9c422ed772
Sam Wood <solemankirby@gmail.com>
2017-11-10 20:57:21 -0500

    Small fixes to the front and backend of acs-portal to allow for interaction with the Eureka and futureproofing possible callback issue

--- acs-portal/backend/src/main/java/com/revature/acs/portal/AcsPortalApplication.java
+++ acs-portal/backend/src/main/java/com/revature/acs/portal/AcsPortalApplication.java
@@ -2,7 +2,9 @@ package com.revature.acs.portal;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
+@EnableEurekaClient
 @SpringBootApplication
 public class AcsPortalApplication {
 
